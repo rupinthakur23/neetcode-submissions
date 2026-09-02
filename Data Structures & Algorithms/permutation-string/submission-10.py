216acac339945459a202426d567d5326bfe5852b
Rupin Singh Thakur <51837205+rupinthakur23@users.noreply.github.com>
2026-09-01 19:20:15 -0500

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Map, s2Map = [0] * 26, [0] * 26
+        for char in s1:
+            s1Map[ord(char) - ord('a')] +=1
+        
+        l = 0
+
+        for r in range(len(s2)):
+            s2Map[ord(s2[r]) - ord('a')] +=1
+
+            if (r - l + 1) > len(s1):
+                s2Map[ord(s2[l]) - ord('a')] -=1
+                l +=1
+            
+            if s1Map == s2Map:
+                return True
+        
+        return False
