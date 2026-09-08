d7e0b27d24e9618e7f4d1ec8887538f588a1f02b
Rupin Singh Thakur <51837205+rupinthakur23@users.noreply.github.com>
2026-09-07 22:26:04 -0500

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        ajcaencyList = defaultdict(list)
+        for uv, uw in edges:
+            ajcaencyList[uv].append(uw)
+            ajcaencyList[uw].append(uv)  
+
+        nodesMap = [False] * n
+
+        result = 0
+        def dfs(node):
+            if nodesMap[node]:
+                return
+            
+            nodesMap[node] = True
+            
+            for nei in ajcaencyList[node]:
+                if nodesMap[nei]:
+                    continue
+                dfs(nei)
+            return
+
+        for i in range(n):
+            if not nodesMap[i]:
+                result +=1
+                dfs(i)
+        return result
