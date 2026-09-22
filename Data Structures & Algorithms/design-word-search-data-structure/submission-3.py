d8ded8be861d1b8b9fa805737637ed54f1855a99
Rupin Singh Thakur <51837205+rupinthakur23@users.noreply.github.com>
2026-09-22 00:09:52 -0500

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.found = False
+
+class WordDictionary:
+
+    def __init__(self):
+         self.root = TrieNode()
+        
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.found = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j, root):
+            curr = root
+
+            for i in range(j, len(word)):
+                if word[i] == '.':
+                    for node in curr.children.values():
+                        if dfs(i + 1, node):
+                            return True
+                    return False
+                else:
+                    if word[i] not in curr.children:
+                        return False
+                    curr = curr.children[word[i]]
+            return curr.found
+
+        return dfs(0, self.root)
+        
